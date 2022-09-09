8d58f6edaf922970fcb2e827ff539d78366e1197
Renan Maia Fernandes <renan.fernandes@webjump.com.br>
2022-09-09 17:53:38 -0300

    update registration scenarie

--- cypress/factories/registrationFactory.js
+++ cypress/factories/registrationFactory.js
@@ -1,20 +1,17 @@
-var faker = require('faker')
+import { faker } from '@faker-js/faker';
 
 export default {
-
     user: function () {
-
         var firstName = faker.name.firstName()
         var lastName = faker.name.lastName()
-
-
+        var password = faker.internet.password()
         var dataPersonal = {
             name: `${firstName}`,
             lastname: `${lastName}`,
             singInInformation: {
                 email: faker.internet.email(firstName),
-                password: 'Nicolecazarini123',
-                confirmPassword:'Nicolecazarini123'
+                password: `${password}`,
+                confirmPassword: `${password}`
             },
         }
         return dataPersonal
--- cypress/pages/registrationPage.js
+++ cypress/pages/registrationPage.js
@@ -1,31 +1,19 @@
-
 class registrationPage {
-
     fillFormNames(user) {
         cy.get('.base').should('have.text', 'Create New Customer Account')
         cy.get('#firstname').type(user.name)
         cy.get('#lastname').type(user.lastname)
     }
-
     fillFormSigninInformation(user) {
         cy.get('#email_address').type(user.singInInformation.email)
         cy.get('#password').type(user.singInInformation.password)
         cy.get('#password-confirmation').type(user.singInInformation.confirmPassword)
-
     }
-
     submit() {
         cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-
     }
-
-    messageSucess(expectedMessage){
-        cy.get('.message-success > div').contains(expectedMessage)
+    messageSucess(expectedMessage) {
+        cy.get('.message-success > div').should('have.text', expectedMessage)
     }
-        
-
-
-
 }
-
 export default new registrationPage
