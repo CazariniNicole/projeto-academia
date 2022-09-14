8fcdc47aaadba870fa8cd69e1e31843f8f589e5d
CazariniNicole <cazarininicole@gmail.com>
2022-09-14 17:09:27 -0300

    update

--- cypress/support/pages/checkoutPage.js
+++ cypress/support/pages/checkoutPage.js
@@ -5,10 +5,10 @@ class checkoutPage {
 
     checkout() {
         cy.get(CheckoutElements.shippingAddress()).contains('Shipping Address')
-        cy.get(CheckoutElements.bottonexemp1).click()
+        cy.get(CheckoutElements.bottonexemp1()).click()
         cy.get(CheckoutElements.bottonexemp2()).click()
         cy.get(CheckoutElements.textPaymentMethod()).should('have.text', 'Payment Method')
-        cy.get(CheckoutElements.bottonexemp3).click()
+        cy.get(CheckoutElements.bottonexemp3()).click()
     } 
 }
 export default checkoutPage
