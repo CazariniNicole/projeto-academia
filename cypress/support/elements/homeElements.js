afdcfea891975709ba1cefe8ca5847f1d96ac1c4
Renan Maia Fernandes <renan.fernandes@webjump.com.br>
2022-09-22 13:05:34 -0300

    Update homeElements.js

--- cypress/support/elements/homeElements.js
+++ cypress/support/elements/homeElements.js
@@ -3,6 +3,6 @@ class homeElements {
     buttonSignIn = () => { return '.panel > .header > .link > a'}
     searchField = () => { return '#search'}
     bottonSearch  = () => { return '#search_mini_form div button'}
-    bottonCreateAccount = () => { return '.panel > .panel > .header > li > #idpckCCbJ6'}
+    bottonCreateAccount = () => { return '.panel > .header > :nth-child(3) > a[href="https://magento.nublue.co.uk/customer/account/create/"]'}
 }
 export default homeElements;
