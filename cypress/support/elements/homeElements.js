5eb8c7cb97b8c943e560f475e29b0369c64c23fc
Renan Maia Fernandes <renan.fernandes@webjump.com.br>
2022-09-22 11:30:30 -0300

    Update homeElements.js

--- cypress/support/elements/homeElements.js
+++ cypress/support/elements/homeElements.js
@@ -3,6 +3,6 @@ class homeElements {
     buttonSignIn = () => { return '.panel > .header > .link > a'}
     searchField = () => { return '#search'}
     bottonSearch  = () => { return '#search_mini_form div button'}
-    bottonCreateAccount = () => { return '.panel > .header > :nth-child(3)'}
+    bottonCreateAccount = () => { return '#idpckCCbJ6'}
 }
 export default homeElements;
